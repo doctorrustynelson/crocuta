724956883652eb279e805a3b2f46c746f9da0840
Rusty Nelson <doctor.rusty.nelson@gmail.com>
2014-12-15 22:13:40 -0500

    Fixed local integration tests so it can run standalone

--- tests/local-integration-tests.js
+++ tests/local-integration-tests.js
@@ -1,6 +1,35 @@
-/**
- * New node file
+/* Node unit quick reference:
+ * 
+ *	ok(value, [message]) 
+ *		- Tests if value is a true value.
+ *	equal(actual, expected, [message]) 
+ *		- Tests shallow, coercive equality with the equal comparison operator ( == ).
+ *	notEqual(actual, expected, [message]) 
+ *		- Tests shallow, coercive non-equality with the not equal comparison operator ( != ).
+ *	deepEqual(actual, expected, [message]) 
+ *		- Tests for deep equality.
+ *	notDeepEqual(actual, expected, [message]) 
+ *		- Tests for any deep inequality.
+ *	strictEqual(actual, expected, [message]) 
+ *		- Tests strict equality, as determined by the strict equality operator ( === )
+ *	notStrictEqual(actual, expected, [message]) 
+ *		- Tests strict non-equality, as determined by the strict not equal operator ( !== )
+ *	throws(block, [error], [message]) 
+ *		- Expects block to throw an error.
+ *	doesNotThrow(block, [error], [message]) 
+ *		- Expects block not to throw an error.
+ *	ifError(value) 
+ *		- Tests if value is not a false value, throws if it is a true value.
+ *	
+ *	expect(amount) 
+ *		- Specify how many assertions are expected to run within a test. 
+ *	done() 
+ *		- Finish the current test function, and move on to the next. ALL tests should call this!
  */
+
+var path = require( 'path' );
+var config = require( '../lib/utils/config' );
+config.reinitialize( path.resolve( __dirname, './config/test.config.json' ) );
 
 var Shenzi = require( '../lib/shenzi-server.js' );
 var Banzai = require( '../lib/banzai-server.js' );
@@ -17,16 +46,37 @@ module.exports.setUp = function( callback ){
 	callback( );
 };
 
-module.exports.tearDown = function( callback ){
-	ed.stop( );
-	banzai.stop( );
-	shenzi.stop( );
+module.exports.setUp = function( callback ){
+	shenzi = new Shenzi( );
+	banzai = new Banzai( );
+	ed = new Ed( );
 	callback( );
 };
 
-module.exports.simpleTest = function( unit ){
+module.exports.simpleTests = {
 	
-	setTimeout( function( ){
-		unit.done( );
-	}, 2000 );
+	printJob: function( unit ){
+		var timeout = setTimeout( function( ){
+			unit.ok( false, 'Test Timed Out.' );
+			unit.done();
+		}, 10000 );
+		
+		var Crocuta = require( '../lib/client' );
+		var crocuta = new Crocuta( );
+		
+		var job = crocuta.createJob( 'hello world' ).joule( function( input, output, reporter ){
+			console.log( arguments );
+			console.log( 'Hello Crocuta!' );
+			return 'Hello User!';
+		} );
+	
+		crocuta.onReady( function( ){
+			unit.ok( true, 'onReady fired.' );
+			job.send( function( result ){
+				unit.equal( result, 'Hello User!', 'Job returned correct result.' );
+				clearTimeout( timeout );
+				unit.done( );
+			} );
+		} );
+	}
 };
